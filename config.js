77566290e896460a5400d84649f4b9df1fc691ef
SmashAvax <andrewareid9@gmail.com>
2021-09-20 17:18:10 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+module.exports = {
+  deployments: {
+    netId56: {
+      ada: {
+        instanceAddress: {
+          '1': '',
+          '10': '',
+          '100': '',
+          '500': '',
+          '1000': ''
+        },
+        tokenAddress: '0x3ee2200efb3400fabb9aacf31297cbdd1d435d47',
+        symbol: 'ADA',
+        decimals: 18
+      }
+    }
+  }
+}
